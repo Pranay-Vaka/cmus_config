f5a89fcbd3dd4daa09d7184a8fef1d6afc19da99
Pranay-Vaka <vakapranaya05@gmail.com>
2023-12-29 14:48:53 +0000

    First commit that has my themes

---
+++ lib.pl
@@ -0,0 +1,23 @@
+/home/pranay/Media/Music/Alec Benjamin - Let Me Down Slowly (Lyrics).m4a
+/home/pranay/Media/Music/Alec Benjamin - The Hill I Will Die On (Lyrics).m4a
+/home/pranay/Media/Music/Boney M - Rasputin (Lyrics).m4a
+/home/pranay/Media/Music/Coldplay - Viva la Vida (Lyrics).m4a
+/home/pranay/Media/Music/Ed Sheeran - Bad Habits (Lyrics).m4a
+/home/pranay/Media/Music/Frank's Choice.m4a
+/home/pranay/Media/Music/I Gave You All.m4a
+/home/pranay/Media/Music/Joji - Glimpse of Us (Lyrics).m4a
+/home/pranay/Media/Music/La Casa De Papel - Bella Ciao [Lyrics] (Money Heist).m4a
+/home/pranay/Media/Music/MASN - Hate Me! (Lyrics) [ffCq7jhmIao].mp3
+/home/pranay/Media/Music/Message to Bears - Find Our Way Home.m4a
+/home/pranay/Media/Music/Mumford and Sons - Broken Crown (Lyrics).m4a
+/home/pranay/Media/Music/OneRepublic - Counting Stars (Lyrics).m4a
+/home/pranay/Media/Music/Rag'n'Bone Man - Human (Lyrics).m4a
+/home/pranay/Media/Music/Rare Americans - Baggage (Official Lyric Video).m4a
+/home/pranay/Media/Music/Rare Americans – Gas Mask (Official Music Video).m4a
+/home/pranay/Media/Music/SLOW DANCING IN THE DARK.m4a
+/home/pranay/Media/Music/Sofia.m4a
+/home/pranay/Media/Music/The Chainsmokers & Coldplay - Something Just Like This (Lyrics).m4a
+/home/pranay/Media/Music/twenty one pilots - Heathens ( Lyrics ).m4a
+/home/pranay/Media/Music/Twenty One Pilots - Ride (Lyrics).m4a
+/home/pranay/Media/Music/Vietsub ｜ Way Down We Go - KALEO ｜ Lyrics Video.m4a
+/home/pranay/Media/Music/Wonderful Life.m4a
